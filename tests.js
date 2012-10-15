f3d8341e28e39cf7d85396eda891a958ff177532
Sean Helvey <smh436@nyu.edu>
2012-10-15 09:49:24 -0600

    modified tests

--- tests.js
+++ tests.js
@@ -1,41 +1,48 @@
-//Bark Testing
-var dog1 = new Dog('woof', "purple");
-test( "dog1 bark test", function() {
-  ok( dog1.bark() == "woof i'm a purple dog", "Passed!" );
+//Dog #1
+var dog1a = new Dog('woof', "purple");
+
+test( "dog1a bark test", function() {
+  ok( dog1a.bark() == "woof i'm a purple dog", "Passed!" );
 });
 
-var dog2 = new Dog('weef', 'green');
-test( "dog2 bark test", function() {
-  ok( dog2.bark() == "weef i'm a green dog", "Passed!" );
+var dog1b = Object.create(Dog.prototype,{"sound" : {value: "woof"},
+										 "color" : {value: "purple"}})
+test( "dog1b bark test", function() {
+  ok( dog1b.bark() == "woof i'm a purple dog", "Passed!" );
 });
 
-var dog3 = new Dog('wurphumph', "orange");
-test( "dog3 bark test", function() {
-  ok( dog3.bark() == "wurphumph i'm a orange dog", "Passed!" );
+//Dog #2
+var dog2a = new weinerDog('weeen', "yellow", 46);
+
+test( "dog2a bark test", function() {
+  ok( dog2a.bark() == "weeen i'm a 46 inch yellow dog", "Passed!" );
 });
 
-var dog4 = new weinerDog('weeen', "yellow", 46);
-test( "dog4 bark test", function() {
-  ok( dog4.bark() == "weeen i'm a 46 inch yellow dog", "Passed!" );
+var dog2b = Object.create(weinerDog.prototype,{"sound" : {value: "weeen"},
+										 "color" : {value: "yellow"},
+										 "length" : {value: 46}})
+test( "dog2b bark test", function() {
+  ok( dog2b.bark() == "weeen i'm a 46 inch yellow dog", "Passed!" );
 });
 
-//NEW vs Object.create
-var dog5 = new Dog("hoowoorf", "red");
-test( "dog5 prototype test", function() {
-  ok( typeof dog5.prototype == "undefined", "Passed!" );
+//Dog #3
+var dog3a = new Dog("hoowoorf", "red");
+test( "dog3a prototype test", function() {
+  ok( typeof dog3a.prototype == "undefined", "Passed!" );
 });
 
-var dog6 = Object.create(Dog);
-test( "dog6 prototype test", function() {
-  ok( typeof dog6.prototype == "object", "Passed!" );
+var dog3b = Object.create(Dog);
+test( "dog3b prototype test", function() {
+  ok( typeof dog3b.prototype == "object", "Passed!" );
 });
 
-var dog7 = new weinerDog("woofskeee", "magenta");
-test( "dog7 prototype test", function() {
-  ok( typeof dog7.prototype == "undefined", "Passed!" );
+//Dog #4
+var dog4a = new weinerDog("woofskeee", "magenta");
+test( "dog4a prototype test", function() {
+  ok( typeof dog4a.prototype == "undefined", "Passed!" );
 });
 
-var dog8 = Object.create(weinerDog);
-test( "dog8 prototype test", function() {
-  ok( typeof dog8.prototype == "object", "Passed!" );
+var dog4b = Object.create(weinerDog);
+test( "dog4b prototype test", function() {
+  ok( typeof dog4b.prototype == "object", "Passed!" );
 });
